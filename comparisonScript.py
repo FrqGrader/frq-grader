c6f76544a1870955201f01bdf4c26b3f266977de
Andrewwu73 <andrewswu2000@gmail.com>
2019-09-14 16:24:37 -0400

    Create comparisonScript.py

---
+++ comparisonScript.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 14 15:09:32 2019
+
+@author: boles
+"""
+
+import requests
+import json
+
+url_base = 'https://www.instabase.com/api/v1/drives'
+root = 'jsu/GraderWorkspace/fs/Instabase%20Drive/files/'
